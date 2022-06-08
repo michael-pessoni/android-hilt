d26329f435e86beeb989798947b3cd5362a272b1
michael-pessoni <pessoni94@gmail.com>
2022-06-08 10:45:30 -0300

    Remove Service Locator

--- app/src/main/java/com/example/android/hilt/LogApplication.kt
+++ app/src/main/java/com/example/android/hilt/LogApplication.kt
@@ -21,11 +21,4 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class LogApplication : Application() {
-
-    lateinit var serviceLocator: ServiceLocator
-
-    override fun onCreate() {
-        super.onCreate()
-        serviceLocator = ServiceLocator(applicationContext)
-    }
 }
